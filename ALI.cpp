b7531dbb87fa832a7a25c266e859bb67a3258e4d
ILUMINUZ <159297397+ILUMINUZ@users.noreply.github.com>
2024-02-07 13:48:46 -0400

    Add files via upload

--- ALI.cpp
+++ ALI.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 
-int main() 
-{
-	std::count<<"Hola Mundo desde UPDS"
-		
+int main() {
+	
+	double num1, num2, num3;
+	
+	std::cout << "Ingrese el primer número: ";
+	std::cin >> num1;
+	
+	std::cout << "Ingrese el segundo número: ";
+	std::cin >> num2;
+	
+	std::cout << "Ingrese el tercer número: ";
+	std::cin >> num3;
+	
+	
+	double mayor = num1;
+	
+	if (num2 > mayor) {
+		mayor = num2;
+	}
+	
+	if (num3 > mayor) {
+		mayor = num3;
+	}
+	
+	
+	std::cout << "El número mayor es: " << mayor << std::endl;
+	
 	return 0;
 }
-
